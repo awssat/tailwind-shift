a14059fe12917a5675498fdec0ef05c216bc2eec
Abdulrahman <saanina@gmail.com>
2019-04-26 03:13:05 +0300

    fixes failed repeated regex

---
+++ tests/SearchAndReplaceTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Awssat\TailwindShift\Test;
+
+use Awssat\TailwindShift\searchAndReplace;
+use PHPUnit\Framework\TestCase;
+
+class SearchAndReplaceTest extends TestCase
+{
+    /** @var \Awssat\TailwindShift\SearchAndReplace */
+    protected $searchAndReplace;
+
+    protected function setUp()
+    {
+        //TODO: phpUnit 8 incompatibility, void error.
+        $this->searchAndReplace = new searchAndReplace();
+    }
+
+    /** @test */
+    public function it_respects_repeated_regex_search_param()
+    {
+        $this->searchAndReplace
+        ->setContent('<tag class="life-is-sad"></tag>')
+        ->perform('{regex_string}-{regex_string}-sad', 
+                '{regex_string}-{regex_string}-fun', 
+                SearchAndReplace::INSIDE_CLASSE_PROP
+            );
+
+        $this->assertEquals(
+            '<tag class="life-is-fun"></tag>',
+            $this->searchAndReplace->get()
+        );
+    }
+
+    /** @test */
+    public function it_respects_repeated_regex_replace_param()
+    {
+        $this->searchAndReplace
+        ->setContent('<tag class="life-is-sad"></tag>')
+        ->perform('life-is-{regex_string}', 
+                '{regex_string}-is-{regex_string}', 
+                SearchAndReplace::INSIDE_CLASSE_PROP
+            );
+
+        $this->assertEquals(
+            '<tag class="sad-is-sad"></tag>',
+            $this->searchAndReplace->get()
+        );
+    }
+}
